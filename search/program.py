33913968b1a1d26149f19da82699de85264b2ebc
legityif <yifanyang80@gmail.com>
2023-03-26 15:29:39 +1100

    optimisations

--- search/program.py
+++ search/program.py
@@ -22,76 +22,61 @@
 # print(render_board(input, ansi=True))
 
 import heapq
-import time
-from search import node
+from .node import *
 
 BOARD_SIZE = 7
-OFFSETS = [(0,1), (-1,1), (-1,0), (0,-1), (1,-1), (1,0)]
+OFFSETS = ((0,1), (-1,1), (-1,0), (0,-1), (1,-1), (1,0))
 
-def search(input: dict[tuple, tuple]) -> list[tuple]:
+def search(input):
     """
     Performs a A* search with a heuristic function on the given board.
     """
-    start_time = time.time()
-
-    # set initial state and append to priority queue
     initial_board = boardify(input)
-    initial_node = node.Node(initial_board, 0, heuristic(initial_board), [])
+    initial_node = Node(initial_board, 0, heuristic(initial_board), [])
     queue = [initial_node]
 
-    # keep track of visited states of the board
     visited = set(tupleify(initial_board))
-    
-    # search until we've found a solution
     while queue:
-        # heapify queue to find minimum cost node
         heapq.heapify(queue)
         curr = heapq.heappop(queue)
-        # print(curr)
-        # if we've found a solution, return it
+        print(curr.moves)
+                
         if get_score(curr.board, 'b', BOARD_SIZE)==0:
-            end_time = time.time()
-            runtime = end_time - start_time
-            print("Runtime:", runtime)
             return curr.moves
         
-        # iterate through all cells in the current state
         for r in range(BOARD_SIZE):
             for c in range(BOARD_SIZE):
-                # find red cells
                 if curr.board[r][c] and curr.board[r][c][0]=="r":
-                    # for every direction in the possible moveset
                     for dr, dc in OFFSETS:
                         curr_child_board = [row[:] for row in curr.board]
                         power = curr.board[r][c][1]
-                        old_r = r
-                        old_c = c
-                        # execute possible move with designated power
-                        while power>0:
-                            new_r, new_c = (r+dr)%BOARD_SIZE, (c+dc)%BOARD_SIZE
-                            # if cell being spread into is empty
-                            if curr_child_board[new_r][new_c]==None:
-                                curr_child_board[new_r][new_c] = ("r", 1)
-                            else:
-                                # check if cell being spread into would result in a power more than 6
-                                if curr_child_board[new_r][new_c][1]+1 >6:
-                                    curr_child_board[new_r][new_c] = None
-                                else:
-                                    # if existing player on cell, spread and add to existing power
-                                    curr_child_board[new_r][new_c] = ("r", curr_child_board[new_r][new_c][1]+1)
-                            r, c = new_r, new_c
-                            power-=1
-                        # set original cell to None after spreading
-                        curr_child_board[old_r][old_c] = None
-                        r, c = old_r, old_c
-                        # append current set of moves to previous set
+                        old_r, old_c = r, c
+                        spread_in_dir(curr_child_board, dr, dc, r, c, old_r, old_c, power)
                         curr_move_copy = curr.moves+[[r, c, dr, dc]]
-                        child_node = node.Node(curr_child_board, curr.path_cost+1, heuristic(curr_child_board), curr_move_copy)
-                        # append current state to priority queue if not visited
+                        child_node = Node(curr_child_board, curr.path_cost+1, heuristic(curr_child_board), curr_move_copy)
                         if tupleify(child_node.board) not in visited:
                             queue.append(child_node)
                             visited.add(tupleify(child_node.board))
 
+def spread_in_dir(curr_child_board, dr, dc, r, c, old_r, old_c, power):
+    """
+    spreads a cell in the given direction.
+    returns: 
+        void
+    """
+    while power>0:
+        new_r, new_c = (r+dr)%BOARD_SIZE, (c+dc)%BOARD_SIZE
+        if curr_child_board[new_r][new_c]==None:
+            curr_child_board[new_r][new_c] = ("r", 1)
+        else:
+            if curr_child_board[new_r][new_c][1]+1 > 6:
+                curr_child_board[new_r][new_c] = None
+            else:
+                curr_child_board[new_r][new_c] = ("r", curr_child_board[new_r][new_c][1]+1)
+        r, c = new_r, new_c
+        power-=1
+    curr_child_board[old_r][old_c] = None
+    r, c = old_r, old_c
 
 def heuristic(board):
     """
@@ -107,7 +92,6 @@ def heuristic(board):
                     total_blue += 1
     return total_blue
 
-
 def boardify(input):
     """
     converts a dictionary of board cell states to a 2D list of lists
@@ -120,16 +104,13 @@ def boardify(input):
         board[r][c] = (player, power)
     return board
 
-
 def tupleify(board):
     """
     converts a 2D list of lists to a dictionary of board cell states
     returns:
         dictionary of board cell states
     """
-    board_tuple = tuple(tuple(row) for row in board)
-    return board_tuple
-        
+    return tuple(tuple(row) for row in board)
 
 def get_score(board, player, board_size):
     """ 
